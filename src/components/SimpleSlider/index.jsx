0875d7e8ff28a6309d8999ecbcb4c18e7963e5ad
Tanmay Dureja <tanmaydureja@gmail.com>
2023-09-08 08:46:18 -0700

    Add images in slider

--- src/components/SimpleSlider/index.jsx
+++ src/components/SimpleSlider/index.jsx
@@ -1,41 +1,40 @@
 import Slider from "react-slick";
 import { Component } from "react";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import image1 from "../../assets/home/1.jpg";
+import image2 from "../../assets/home/2.jpg";
+import image3 from "../../assets/home/3.jpg";
+import image4 from "../../assets/home/4.jpg";
+
 export default class SimpleSlider extends Component {
-    render() {
-      const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
-      return (
-        <div>
-          <h2> Single Item</h2>
-          <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-          </Slider>
-        </div>
-      );
-    }
+  render() {
+    const settings = {
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <Slider {...settings}>
+          <div>
+            <img src={image1} alt="image1" className="rounded-md p-8 bg-white border-2 border-white" />
+          </div>
+          <div>
+            <img src={image2} alt="image2" className="rounded-md p-8 bg-white border-2 border-white" />
+          </div>
+          <div>
+            <img src={image3} alt="image3" className="rounded-md p-8 bg-white border-2 border-white" />
+          </div>
+          <div>
+            <img src={image4} alt="image4" className="rounded-md p-8 bg-white border-2 border-white" />
+          </div>
+        </Slider>
+      </div>
+    );
   }
+}
